46a47c81fd6271902b344f83a92c071f4f870362
AvateNavnath <168811312+AwateNavnath@users.noreply.github.com>
2025-05-14 21:59:10 +0530

    Add files via upload

---
+++ Addtion.java
@@ -0,0 +1,11 @@
+class Add {
+ public static void main(string [] args) {
+     int num1,num2;
+system.out.println("Enter first number : ");
+Scanner sc = new Scanner(System.in);
+num1 = sc.nextInt();
+System.out.println("Enter second number : ");
+num2 = sc.nextInt();
+System.out.println(sum of these number is :" + (num1 +num2));
+    }
+} 
